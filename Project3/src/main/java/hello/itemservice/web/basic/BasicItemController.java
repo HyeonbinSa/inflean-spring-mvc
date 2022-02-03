61bfdbed207f9f148571f8aa316e130cbe3fad60
HyeonbinSa <shb1833@gmail.com>
2022-02-03 12:58:16 +0900

    feat: 상품 등록 기능 구현(2) - Controller 상품 저장 기능 구현

--- Project3/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Project3/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -45,12 +42,54 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    // 상품 등록 처리 
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    // 상품 등록 처리
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        // item 상세 화면으로 이동
+        return "basic/item";
     }
 
+    // 상품 등록 처리
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+
+        // @ModelAttribute 역할
+        // 1. 요청파라미터 처리
+        // 2. Model에 객체를 자동으로 넣어줌.
+        // model.addAttribute("item", item);
+
+        // item 상세 화면으로 이동
+        return "basic/item";
+    }
+
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model) {
+        // Default : Item -> item 가 model attribute 에 담기게된다.
+
+        itemRepository.save(item);
+
+        // @ModelAttribute 역할
+        // 1. 요청파라미터 처리
+        // 2. Model에 객체를 자동으로 넣어줌.
+        // model.addAttribute("item", item);
+
+        // item 상세 화면으로 이동
+        return "basic/item";
+    }
     /**
      * Test Data 추가
      */
