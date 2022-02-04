44406897948cd4d40940720d9c53014da7e7a888
HyeonbinSa <shb1833@gmail.com>
2022-02-04 14:56:55 +0900

    feat: 상품 수정 기능 구현

--- Project3/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Project3/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -90,6 +90,20 @@ public class BasicItemController {
         // item 상세 화면으로 이동
         return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      * Test Data 추가
      */
